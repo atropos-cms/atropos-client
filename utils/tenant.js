3270e24b33d65a39e1b2d05247b37b239efae3fd
Ralph Huwiler <ralph@huwiler.rocks>
2018-08-28 17:39:39 +0200

    fix a context leak issue when getting tenant

--- utils/tenant.js
+++ utils/tenant.js
@@ -1,20 +1,13 @@
-let cachedTenant = null
-
 export function getTenant (req) {
   if (process.env.TENANT_ID) return process.env.TENANT_ID
 
-  let hostname = cachedTenant
-
-  if (!hostname) {
-    if (req && process.server) {
-      hostname = req.headers.host.replace(/:([0-9]*)/, '')
-    }
-
-    if (!req && process.client) {
-      hostname = `${window.location.hostname}`
-    }
-    cachedTenant = hostname
+  if (req && process.server) {
+    return req.headers.host.replace(/:([0-9]*)/, '')
   }
 
-  return (hostname || '')
+  if (!req && process.client) {
+    return `${window.location.hostname}`
+  }
+
+  return ''
 }
