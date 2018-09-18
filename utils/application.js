0d4d3473a54f923be97bb63f024f999f7f7cc83e
Ralph Huwiler <ralph@huwiler.rocks>
2018-09-18 20:04:16 +0200

    remove debug from build

--- utils/application.js
+++ utils/application.js
@@ -1,6 +1,5 @@
 import { i18nInstance } from '~/plugins/i18n'
 import moment from 'moment'
-const debug = require('debug')('atropos:application')
 
 export function resetApplicationSetting () {
   updateApplicationLocale(process.env.LANG)
@@ -13,7 +12,6 @@ export function updateApplicationSettings (settings) {
 export function updateApplicationLocale (locale) {
   // set localization for i18n if set
   if (locale && i18nInstance.locale !== locale) {
-    debug(`Setting locale to ${locale}`)
     i18nInstance.locale = locale
     moment.locale(locale)
   }
